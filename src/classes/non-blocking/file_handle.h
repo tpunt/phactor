e7a3d848d22bcc3ef3d9b077e8527843bae1edad
Thomas Punt <tpunt@php.net>
2018-04-21 16:08:17 +0100

    Cleanup

--- src/classes/non-blocking/file_handle.h
+++ src/classes/non-blocking/file_handle.h
@@ -25,16 +25,13 @@
 
 typedef struct _ph_file_handle_t {
     uv_fs_t fs;
-    // uv_fs_event_t event;
     uv_pipe_t file_pipe;
     uv_write_t write;
     char *name;
     char *buffer;
-    int fd;
+    uv_file fd;
     zend_long buffer_size;
     zend_long file_size;
-    // zend_function *read_function;
-    // zend_function *monitor_function;
     ph_string_t actor_ref;
     int actor_restart_count; // maintains actor version number
     zend_object obj;
